e56cfc68e62f1152b47fe0fd0a1a706e0ab890f5
Jake Shilling <shilling.jake@gmail.com>
2019-10-08 16:50:24 -0400

    updating system

--- 00-linux-virtual-machine/xubuntu/setup.sh
+++ 00-linux-virtual-machine/xubuntu/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "$(whoami)"
-
+# If this script is not being run as root, re-launch with sudo
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+
+# Make sure that the system is up to date
+apt-get -yq update && apt-get -yq upgrade
